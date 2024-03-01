b1d21b6d4142c746b66f69a9f6098ba11ffde132
smit2308 <62828738+smit2308@users.noreply.github.com>
2024-02-29 21:17:03 -0800

    reconfiguration of cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://dora-ai.vercel.app',
+    methods: ['GET', 'POST'], // Add the methods you need
+    allowedHeaders: ['Content-Type', 'Authorization'], // Add the headers you need
+  }));
 app.use(express.json({ limit: '50mb' }));
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
