c58655fd52b9b6862deb52fdfbff88fb1faa037b
smit2308 <62828738+smit2308@users.noreply.github.com>
2024-02-29 21:27:11 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,9 +11,9 @@ dotenv.config();
 const app = express();
 // changes here
 app.use(cors({
-    origin: 'https://dora-ai.vercel.app',
-    methods: ['GET', 'POST'], // Add the methods you need
-    allowedHeaders: ['Content-Type', 'Authorization'], // Add the headers you need
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   }));
 app.use(express.json({ limit: '50mb' }));
 app.use('/api/v1/post', postRoutes);
