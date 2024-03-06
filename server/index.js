e8c460514d73eb24d9ade67dcbf51154d6ebda64
smit2308 <62828738+smit2308@users.noreply.github.com>
2024-03-05 20:12:35 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,20 +11,15 @@ dotenv.config();
 const app = express();
 // changes here
 // CORS configuration
-app.use((req, res, next) => {
-    
-    if (req.method === 'OPTIONS') {
-        return req.method
-        return res.sendStatus(200); // Respond to preflight requests
-    }
-    next();
-});
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
-    res.send('Hello From dall e!');
+    res.status(200).json({
+        message: 'Hello from DALL.E!',
+      });
     });
 
 const startServer = async () => {
