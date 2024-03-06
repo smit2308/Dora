c7166f8e442638bac394c52d396f43758ee12461
smit2308 <62828738+smit2308@users.noreply.github.com>
2024-03-06 12:08:33 -0800

    Final working

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true)
       try {
-        const response = await fetch('https://doraserver.vercel.app/api/v1/post',
+        const response = await fetch('https://dora-ylqx.onrender.com/api/v1/post',
         {
           method: 'GET',
           headers: {
