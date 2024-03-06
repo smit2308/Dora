021e6aa30370b47275df5c63eb8ac5323bd4e12c
smit2308 <62828738+smit2308@users.noreply.github.com>
2024-03-05 21:51:47 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,11 +15,12 @@ app.use(express.json({ limit: '50mb' }));
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
-app.get('/', async (req, res) => {
-    res.status(200).json({
-        message: 'Hello from DALL.E!',
-      });
-    });
+app.get("/", async (req, res) => {
+    res.send("Hello from DALL-E Cloneaazzzzaaaa");
+  });
+  
+  // Handle OPTIONS requests
+  app.options("*", cors()); 
 
 const startServer = async () => {
 
