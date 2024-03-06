d60631506bb2d1a65911b72bfe6f65cacd820020
smit2308 <62828738+smit2308@users.noreply.github.com>
2024-03-05 21:46:57 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,16 +9,7 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-// changes here
-// CORS configuration
-// CORS configuration
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
+  app.use(cors());
   
 app.use(express.json({ limit: '50mb' }));
 app.use('/api/v1/post', postRoutes);
