ceb50a9805250ff5978bcf312e084c99d755c6de
smit2308 <62828738+smit2308@users.noreply.github.com>
2024-03-05 23:32:26 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,23 +9,17 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-  app.use(cors());
-  
+
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
-  // Handle OPTIONS requests for all routes
-app.get("/", async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
 
-    if(req.method === 'OPTIONS') { return res.status(200).json(({ body: "OK" })) }
-    // res.send("Hello from DALL-E Cloneaazzzzaaaa");
-  });
-  
-
+app.get('/', async (req, res) => {
+    res.status(200).json({
+        message: 'Hello from DALL.E!',
+      });
+    });
 
 const startServer = async () => {
 
