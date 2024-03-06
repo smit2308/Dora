08ba33c519976e914c7f2258a609f25555b1a17d
smit2308 <62828738+smit2308@users.noreply.github.com>
2024-03-05 23:20:56 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,9 +15,14 @@ app.use(express.json({ limit: '50mb' }));
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
   // Handle OPTIONS requests for all routes
-  app.options("*", cors()); 
 app.get("/", async (req, res) => {
-    res.send("Hello from DALL-E Cloneaazzzzaaaa");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
+
+    if(req.method === 'OPTIONS') { return res.status(200).json(({ body: "OK" })) }
+    // res.send("Hello from DALL-E Cloneaazzzzaaaa");
   });
   
 
