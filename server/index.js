255671ac650ebf8b6e7c602ea62df1dd64bc840e
smit2308 <62828738+smit2308@users.noreply.github.com>
2024-07-31 12:51:17 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,12 +10,13 @@ dotenv.config();
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://dora-ai.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
+const corsOptions = {
+    origin: 'https://dora-ai.vercel.app',
+    methods: 'GET, POST, OPTIONS',
+    allowedHeaders: 'Content-Type, Accept'
+};
+
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '50mb' }));
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
@@ -23,20 +24,16 @@ app.use('/api/v1/dalle', dalleRoutes);
 app.get('/', async (req, res) => {
     res.status(200).json({
         message: 'Hello from DALL.E!',
-      });
     });
+});
 
 const startServer = async () => {
-
     try {
-        
-        connectDB(process.env.MONGODB_URL);
-        app.listen(8080, () =>  console.log('Server is running on port http://localhost:8080'))
-    }
-    catch (error) {
+        await connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () => console.log('Server is running on port http://localhost:8080'));
+    } catch (error) {
         console.log(error);
     }
+};
 
-  
-}
 startServer();
