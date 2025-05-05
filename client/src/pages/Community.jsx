6ffc639a4af365cc1eda1beee3f930c1ab4f7358
Smit Shewale <smitshewale@gmail.com>
2025-05-05 00:11:02 -0400

    Previous

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -33,7 +33,7 @@ const Community = () => {
     const fetchPosts = async () => {
       setLoading(true)
       try {
-        const response = await fetch('https://dora-ylqx.onrender.com/api/v1/post',
+        const response = await fetch('https://dora-server-g8bq.onrender.com/api/v1/post',
         {
           method: 'GET',
           headers: {
