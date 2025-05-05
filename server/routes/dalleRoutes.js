79530af97f6d02515c0c32492687cfc1dedaaf66
Smit Shewale <smitshewale@gmail.com>
2025-05-05 00:57:32 -0400

    Final

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -19,20 +19,34 @@ router.route('/').get((req, res) => {
 router.route('/').post(async (req, res) => {
     try {
         const {prompt, model, quality, style} = req.body;
-      
-        const aiResponse = await openai.images.generate({
+        
+        // console.log(prompt, model, quality, style, "prompt model quality style")
+       
+        let aiResponse;
+
+        if (model === 'dall-e-3') {
+          aiResponse = await openai.images.generate({
             model: model,
             prompt: prompt,
             n: 1,
             size: "1024x1024",
-            response_format: "b64_json",
             quality: quality,
             style: style
+         
           });
-          
+        } else {
+          aiResponse = await openai.images.generate({
+            model: model,
+            prompt: prompt,
+            n: 1,
+            size: "1024x1024",
+            response_format: "b64_json"
+          });
+        }
 
         const image = aiResponse.data[0].b64_json;
         res.status(200).json({image: image});
+        // console.log(image, "image generated");
     } catch (error) {
         console.log(error);
         // res.status(500).send(error?.response.data.error.message)
